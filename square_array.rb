4cda7c7000ddb7f7ad99eb7798623e421775353a
Irina Bright <missirinabright@gmail.com>
2019-05-24 14:43:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared_array = []
-  array.each {|element| square_array << 
+  array.each {|element| square_array << element ** 2}
   square_array.each do 
     puts number ** 2 
   end  
